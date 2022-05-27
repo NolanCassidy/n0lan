caa066c67193283c8eab2fccba5bccda1c9e08d8
Nolan Cassidy <nolancassidy1@Ggmail.com>
2022-05-27 09:52:26 -0700

    add new pipeline with all vars

--- server/src/constants/connection.ts
+++ server/src/constants/connection.ts
@@ -1,12 +1,13 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-export const DEV_FRONTEND = 'http://localhost:3000';
-export const PROD_FRONTEND = 'https://figstack.com';
-export const ENDPOINT_FRONTEND: string = process.env.NODE_ENV === 'development' ? DEV_FRONTEND : PROD_FRONTEND;
+export const DEV_FRONTEND = "http://localhost:3000";
+export const PROD_FRONTEND = "https://n0lan.com";
+export const ENDPOINT_FRONTEND: string =
+  process.env.NODE_ENV === "development" ? DEV_FRONTEND : PROD_FRONTEND;
 
 export const OPENAI_AUTHORIZATION = {
   headers: {
-    Authorization: `Bearer ${process.env.OPENAI_TOKEN}`
-  }
-}
+    Authorization: `Bearer ${process.env.OPENAI_TOKEN}`,
+  },
+};
