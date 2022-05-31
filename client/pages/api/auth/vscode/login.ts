a0f85f2bdbcf180e7ec385bc79e7e5f92994c22d
Nolan Cassidy <nolancassidy1@Ggmail.com>
2022-05-31 11:38:24 -0700

    add another signin

--- client/pages/api/auth/vscode/login.ts
+++ client/pages/api/auth/vscode/login.ts
@@ -10,7 +10,7 @@ const vscodeLogin = async (req: NextApiRequest, res: NextApiResponse) => {
     });
     const { refreshToken, accessToken } = refreshTokenRes.data;
     res.redirect(
-      `vscode://NolanCassidy.vsc/callback?refreshToken=${refreshToken}&accessToken=${accessToken}`
+      `vscode://N0lan.vsc/callback?refreshToken=${refreshToken}&accessToken=${accessToken}`
     );
   } catch (error: any) {
     res.send(error.response.data);
